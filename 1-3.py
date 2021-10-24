b547f80b88bdb061e909a70bfb9254b1572ff1cf
plombir-mary <89405297+plombir-mary@users.noreply.github.com>
2021-10-24 22:36:19 +0300

    Update 1-3.py

--- 1-3.py
+++ 1-3.py
@@ -1,6 +1,6 @@
 # склонение слова процент
-for i in range(1, 101):
-    if i == 11 or i == 12 or i == 13 or i == 14:
+for i in range(0, 235):
+    if i == 11 or i == 12 or i == 13 or i == 14 or i == 111 or i == 112 or i == 113 or i == 114 or i == 211 or i == 212 or i ==213 or i ==214:
         print(f"{i} процентов")
     else:
         if i % 10 == 1:
